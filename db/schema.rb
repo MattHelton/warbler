b58fd24f57fca90fffbc2f3f1ed6f2669ee0aac2
Matthew Helton <mjoelhelton@me.com>
2018-02-01 23:33:17 -0500

    Sets up for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180123012711) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tags", force: :cascade do |t|
     t.string "phrase"
     t.datetime "created_at", null: false
